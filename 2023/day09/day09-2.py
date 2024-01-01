de151c56638c7837a6ef9bd07400f2d47387c440
Jupyo Hong <jupyohong7@gmail.com>
2024-01-01 20:00:59 +0900

    Add day 9

---
+++ 2023/day09/day09-2.py
@@ -0,0 +1,22 @@
+def extrapolate(sequence):
+    if len(sequence) < 2:
+        return sequence[0]
+    new_sequence = []
+    all_zeroes = True
+    for i in range(1, len(sequence)):
+        diff = sequence[i] - sequence[i-1]
+        if diff:
+            all_zeroes = False
+        new_sequence.append(diff)
+    if not all_zeroes:
+        return sequence[0] - extrapolate(new_sequence)
+    return sequence[0] - new_sequence[0]
+
+
+with open('input.txt') as f:
+    histories = f.readlines()
+    result = 0
+    for history in histories:
+        next_value = extrapolate(list(map(int, history.strip().split())))
+        result += next_value
+    print('result:', result)
